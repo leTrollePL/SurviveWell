ce9b06ebef70a5bec2b1979e4aac8df21f13c169
Piotr Strojny <p.strojny@student.po.edu.pl>
2021-04-15 01:36:41 +0200

    Spadające Platformy

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    [SerializeField] float CzasZniknięcia = 2f;
+    [SerializeField] float CzasDoSpadku = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("step on");
+        if (collision.gameObject.tag == "Player")
+        {
+            PlatformManager.Instance.StartCoroutine("SpawnPlatform", new Vector2(transform.position.x, transform.position.y));
+            Invoke("Drop", CzasDoSpadku);
+            Destroy(gameObject, CzasZniknięcia);
+        }
+    }
+
+    void Drop()
+    {
+        rb.isKinematic = false;
+    }
+}
---
+++ Assets/Scripts/PlatformManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour
+{
+    public static PlatformManager Instance = null;
+  
+    [SerializeField] GameObject fallingPlatform;
+    [SerializeField] float czasOdrodzeniaPlatformy = 3f;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate(fallingPlatform, new Vector2(1f,1f), fallingPlatform.transform.rotation);
+
+    }
+
+    IEnumerator SpawnPlatform(Vector2 spawnPosition)
+    {
+        yield return new WaitForSeconds(czasOdrodzeniaPlatformy);
+        Instantiate(fallingPlatform, spawnPosition, fallingPlatform.transform.rotation);
+    }
+}
