650956e43635f4108dba87c90ba3254f6c2b64fd
Adrian Pogrzeba <adipoggger@wp.pl>
2021-04-15 16:07:08 +0200

    Revert "CommitTestowyNiePobierać"
    This reverts commit 93d8136de09976eee55ac454410a1917806557fe.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,13 +6,9 @@ public class Health : MonoBehaviour
 {
     [SerializeField] float health =100f;
 
-    public float maxHealth;
-    public float currentHealth;
-
     public void DealDamage(float damage)
     {
         health -= damage;
-        currentHealth = health;
         if (health <= 0)
         {
             if(gameObject.layer==9 || gameObject.layer == 10)
@@ -28,7 +24,7 @@ public class Health : MonoBehaviour
     }
     void Start()
     {
-        currentHealth = health;
+        
     }
 
     // Update is called once per frame
