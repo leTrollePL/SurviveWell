93d8136de09976eee55ac454410a1917806557fe
Juzefus998 <m.juzefus@student.po.edu.pl>
2021-04-15 15:31:29 +0200

    CommitTestowyNiePobierać
    Nie Pobierać !!! This is not  a  drill!!!!

---
+++ Assets/Scripts/LepszySkok.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LepszySkok : MonoBehaviour
+{
+    WhiteController white;
+    BlackController black;
+
+    public float jumpbonus = 50f;
+
+    void Awake()
+    {
+        white = FindObjectOfType<WhiteController>();
+        black = FindObjectOfType<BlackController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "White")
+        {
+            white.betterjump = true;
+
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.name == "Black")
+        {
+            black.betterjump = true;
+
+            Destroy(gameObject);
+        }
+
+    }
+}
