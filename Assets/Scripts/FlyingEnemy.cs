b597e247d8aa2da6de76463acfa39f5056b4d500
Adrian Pogrzeba <adipoggger@wp.pl>
2021-04-16 00:59:47 +0200

    Latający Strzelający wróg dodany

--- Assets/Scripts/FlyingEnemy.cs
+++ Assets/Scripts/FlyingEnemy.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class FlyingEnemy : Enemy
 {
     // Start is called before the first frame update
-  
+   
 
     // Update is called once per frame
-  public override void Update()
+    public override void Update()
     {
         if (GetComponent<Renderer>().isVisible)
         {
---
+++ Assets/Scripts/FlyingEnemyShooter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemyShooter : Enemy
+{
+    // Start is called before the first frame update
+    public int shotTimer = 0;
+    public int shotReset = 450;
+    public GameObject Bullet;
+    public EnemyBullet eBullet;
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        if (GetComponent<Renderer>().isVisible)
+        {
+
+            Vector3 direction = getShortestPath();
+            if (shotTimer == shotReset)
+            {
+
+
+                Vector2 diff = new Vector2(direction.x, direction.y);
+               // diff = diff / Mathf.Sqrt(diff.x * diff.x + diff.y * diff.y);
+              
+              
+                eBullet.xDiff = direction.x/10;
+                eBullet.yDiff = direction.y/10;
+                GameObject BulletClone = Instantiate(Bullet, GetComponent<Transform>().transform.position, Quaternion.identity);
+                BulletClone.SetActive(true);
+                Debug.Log("shoot");
+                shotTimer = 0;
+            }
+            if (direction.magnitude > 5)
+            {
+                direction /= 45 * direction.magnitude;
+            }
+            else
+            {
+                direction /= -45 * direction.magnitude;
+            }
+            GetComponent<Transform>().position += direction;
+            shotTimer++;
+           
+        }
+    }
+}
