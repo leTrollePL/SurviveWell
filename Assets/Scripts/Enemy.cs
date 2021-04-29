d4e9d7835263dc33492759d7497a46d6db121842
acelpalumbo <62113788+AcelPalumbo@users.noreply.github.com>
2021-04-29 14:12:04 +0200

    zmergowanie- grafiki + mechaniki enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,40 +7,121 @@ public class Enemy : MonoBehaviour
 {
     public GameController GController;
     public GameObject enemyObject;
-    public int damage=20;
+    public int damage = 20;
     public int moneyWorth = 5;
+    public float speed = 0.0f;
+    public float distance = 2f;
+    public Transform GroundDetection;
+    public bool movingRight;
+    public int jumpTimer = 1500, jumpCD = 1500;
+    public int pounceReset = 750, pounceTimer = 0;
+
 
     public virtual void Update()
-    { 
+    {
         if (GetComponent<Renderer>().isVisible)
         {
             Vector3 direction = getShortestPath();
-            if (direction.x > 0)
-                this.GetComponent<SpriteRenderer>().flipX = false;
+
+
+            if (direction.magnitude < 10)
+            {
+                if (direction.x > 0)
+                {
+                    transform.eulerAngles = new Vector3(0, -180, 0);
+                }
+                else
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                RaycastHit2D groundInfo = Physics2D.Raycast(GroundDetection.position, Vector2.down, distance / 100);
+                if (groundInfo.collider == true && groundInfo.collider.gameObject.layer == 8)
+                {
+                    pounceTimer++;
+
+                    if (pounceTimer >= pounceReset)
+                    {
+                        pounceTimer = 0;
+                        this.GetComponent<Rigidbody2D>().AddForce(new Vector3(direction.x, direction.y + 0.5f, 0) / 5, ForceMode2D.Impulse);
+                    }
+                    else if (pounceTimer > pounceReset - 30)
+                    {
+                        this.GetComponent<Transform>().Rotate(0, 0, pounceTimer - pounceReset);
+                    }
+                    else if (pounceTimer > pounceReset - 60)
+                    {
+                        this.GetComponent<Transform>().Rotate(0, 0, pounceReset - 60 - pounceTimer);
+                    }
+                }
+            }
             else
-                this.GetComponent<SpriteRenderer>().flipX = true;
-            direction =direction/ ( 7 * Mathf.Sqrt(direction.x*direction.x+direction.y*direction.y));
-            GetComponent<Rigidbody2D>().AddForce(direction);
+            {
+                RaycastHit2D groundInfo = Physics2D.Raycast(GroundDetection.position, Vector2.down, distance);
+                if (groundInfo.collider == false)
+                {
+                    changeDirection();
+                }
+                else
+                {
+
+                    RaycastHit2D jumpInfo = Physics2D.Raycast(GroundDetection.position, Vector2.left, distance);
+                    if (jumpInfo.collider == true)
+                        if (groundInfo.collider == true && jumpInfo.collider.gameObject.layer == 8 && jumpTimer == jumpCD)
+                        {
+                            this.GetComponent<Rigidbody2D>().AddForce(Vector2.up / 2f, ForceMode2D.Impulse);
+                            jumpTimer = 0;
+                        }
+
+                    RaycastHit2D wallinfo = Physics2D.Raycast(GroundDetection.position, Vector2.left, distance / 1000);
+                    if (wallinfo.collider == true && wallinfo.collider.gameObject.layer == 8)
+                    {
+                        changeDirection();
+                    }
+
+                }
+                if (movingRight == false)
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    this.GetComponent<Transform>().position = new Vector3(this.GetComponent<Transform>().position.x - speed, this.GetComponent<Transform>().position.y, 0);
+                }
+                else
+                {
+                    transform.eulerAngles = new Vector3(0, -180, 0);
+                    this.GetComponent<Transform>().position = new Vector3(this.GetComponent<Transform>().position.x + speed, this.GetComponent<Transform>().position.y, 0);
+                }
+
+                //if (groundInfo.collider == true)
+                //{
+                //    Debug.Log("Ground under feet");
+                //}
+            }
+            if (jumpTimer < jumpCD)
+                jumpTimer++;
         }
     }
+    public void changeDirection()
+    {
+        if (movingRight == true)
+            movingRight = false;
+        else
+            movingRight = true;
+    }
 
-    public Vector3 getShortestPath()
+    public virtual Vector3 getShortestPath()
     {
         Vector3 vector = new Vector3();
         double distW = Vector3.Distance(GController.WController.transform.position, GetComponent<Transform>().position);
         double distB = Vector3.Distance(GController.BController.transform.position, GetComponent<Transform>().position);
 
-        if (distB>distW)
-        vector = (GController.WController.transform.position - GetComponent<Transform>().position);
+        if (distB > distW)
+            vector = (GController.WController.transform.position - GetComponent<Transform>().position);
         else
             vector = (GController.BController.transform.position - GetComponent<Transform>().position);
 
         return vector;
     }
 
-   public void OnTriggerEnter2D(Collider2D collidingObject)
+    public void OnTriggerEnter2D(Collider2D collidingObject)
     {
-        if (collidingObject.gameObject.layer == 9&& GController.WController.GetComponent<WhiteController>().hittable)
+        if (collidingObject.gameObject.layer == 9 && GController.WController.GetComponent<WhiteController>().hittable)
         {
             var health = GController.WController.GetComponent<Health>();
             if (health != null)
@@ -53,22 +134,22 @@ public class Enemy : MonoBehaviour
             GController.WController.GetComponent<WhiteController>().movespeed += diff.x * 0.07f;
         }
         if (collidingObject.gameObject.layer == 10)
+        {
+            var health = GController.BController.GetComponent<Health>();
+            if (health != null)
             {
-                var health = GController.BController.GetComponent<Health>();
-                if (health != null)
-                {
-                    health.DealDamage(damage);
-                }
+                health.DealDamage(damage);
+            }
             Vector2 diff = new Vector2(GController.BController.transform.position.x - GetComponent<Transform>().position.x, GController.BController.transform.position.y - GetComponent<Transform>().position.y);
             diff = diff / Mathf.Sqrt(diff.x * diff.x + diff.y * diff.y);
             GController.BController.GetComponent<Rigidbody2D>().AddForce(GController.BController.transform.up * diff.y * 160);
             GController.BController.GetComponent<BlackController>().movespeed += diff.x * 0.07f;
         }
-           
 
 
 
-      
+
+
 
     }
 
--- Assets/Scripts/FlyingEnemyShooter.cs
+++ Assets/Scripts/FlyingEnemyShooter.cs
@@ -9,44 +9,103 @@ public class FlyingEnemyShooter : Enemy
     public int shotReset = 450;
     public GameObject Bullet;
     public EnemyBullet eBullet;
+    public int playerDistance = 5;
+    public int speedConstraint = 60;
+    public Transform UpPoint, DownPoint, LeftPoint, RightPoint;
 
     // Update is called once per frame
     public override void Update()
     {
         if (GetComponent<Renderer>().isVisible)
         {
-            
-            Vector3 direction = getShortestPath();
-            if (direction.x > 0)
-                this.GetComponent<SpriteRenderer>().flipX = false;
-            else
-                this.GetComponent<SpriteRenderer>().flipX = true;
+
+            Vector3 direction = getShortestPathPlace();
+            Vector3 shotDirection = getShortestPath();
+            //if (direction.x > 0)
+            //    this.GetComponent<SpriteRenderer>().flipX = false;
+            //else
+            //    this.GetComponent<SpriteRenderer>().flipX = true;
             if (shotTimer == shotReset)
             {
 
 
-                Vector2 diff = new Vector2(direction.x, direction.y);
-               // diff = diff / Mathf.Sqrt(diff.x * diff.x + diff.y * diff.y);
-              
-              
-                eBullet.xDiff = direction.x/10;
-                eBullet.yDiff = direction.y/10;
+                Vector2 diff = new Vector2(shotDirection.x, shotDirection.y);
+                // diff = diff / Mathf.Sqrt(diff.x * diff.x + diff.y * diff.y);
+
+
+                eBullet.xDiff = shotDirection.x / 10;
+                eBullet.yDiff = shotDirection.y / 10;
                 GameObject BulletClone = Instantiate(Bullet, GetComponent<Transform>().transform.position, Quaternion.identity);
                 BulletClone.SetActive(true);
-                Debug.Log("shoot");
                 shotTimer = 0;
             }
-            if (direction.magnitude > 5)
+            if (direction.x > 0)
             {
-                direction /= 45 * direction.magnitude;
+                // this.GetComponent<SpriteRenderer>().flipX = true;
+                RaycastHit2D rightInfo = Physics2D.Raycast(RightPoint.position, Vector2.right, distance / 100);
+                if (rightInfo.collider == true && rightInfo.collider.gameObject.layer == 8)
+                {
+                    direction.x = 0;
+                }
             }
             else
             {
-                direction /= -45 * direction.magnitude;
+                //  this.GetComponent<SpriteRenderer>().flipX = false;
+                RaycastHit2D leftInfo = Physics2D.Raycast(LeftPoint.position, Vector2.right, distance / 100);
+                if (leftInfo.collider == true && leftInfo.collider.gameObject.layer == 8)
+                {
+                    direction.x = 0;
+                }
             }
+            if (direction.y > 0)
+            {
+                RaycastHit2D upInfo = Physics2D.Raycast(UpPoint.position, Vector2.right, distance / 100);
+                if (upInfo.collider == true && upInfo.collider.gameObject.layer == 8)
+                {
+                    direction.y = 0;
+                }
+            }
+            else
+            {
+                RaycastHit2D downInfo = Physics2D.Raycast(DownPoint.position, Vector2.right, distance / 100);
+                if (downInfo.collider == true && downInfo.collider.gameObject.layer == 8)
+                {
+                    direction.y = 0;
+                }
+            }
+
+            direction /= speedConstraint * direction.magnitude;
+
             GetComponent<Transform>().position += direction;
             shotTimer++;
-           
+
         }
+    }
+    public Vector3 getShortestPathPlace()
+    {
+        Vector3 vector = new Vector3();
+        double distW = Vector3.Distance(GController.WController.transform.position, GetComponent<Transform>().position);
+        double distB = Vector3.Distance(GController.BController.transform.position, GetComponent<Transform>().position);
+
+        if (distB > distW)
+        {
+            double distW1 = Vector3.Distance(new Vector3(GController.WController.transform.position.x + playerDistance, GController.WController.transform.position.y + playerDistance, 0), GetComponent<Transform>().position);
+            double distW2 = Vector3.Distance(new Vector3(GController.WController.transform.position.x - playerDistance, GController.WController.transform.position.y + playerDistance, 0), GetComponent<Transform>().position);
+            if (distW1 < distW2)
+                vector = new Vector3(GController.WController.transform.position.x + playerDistance, GController.WController.transform.position.y + playerDistance, 0) - GetComponent<Transform>().position;
+            else
+                vector = new Vector3(GController.WController.transform.position.x - playerDistance, GController.WController.transform.position.y + playerDistance, 0) - GetComponent<Transform>().position;
+        }
+        else
+        {
+            double distB1 = Vector3.Distance(new Vector3(GController.BController.transform.position.x + playerDistance, GController.BController.transform.position.y + playerDistance, 0), GetComponent<Transform>().position);
+            double distB2 = Vector3.Distance(new Vector3(GController.BController.transform.position.x - playerDistance, GController.BController.transform.position.y + playerDistance, 0), GetComponent<Transform>().position);
+            if (distB1 < distB2)
+                vector = new Vector3(GController.BController.transform.position.x + playerDistance, GController.BController.transform.position.y + playerDistance, 0) - GetComponent<Transform>().position;
+            else
+                vector = new Vector3(GController.BController.transform.position.x - playerDistance, GController.BController.transform.position.y + playerDistance, 0) - GetComponent<Transform>().position;
+        }
+
+        return vector;
     }
 }
