c78acdf4c118e16cf9f6865e1420e4000822bcc3
Piotr Strojny <p.strojny@student.po.edu.pl>
2021-04-30 01:05:13 +0200

    NiszczenieScianyIUkrytyPokuj

---
+++ Assets/Scripts/UkrytyPokuj.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UkrytyPokuj : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        //Biały/niebieski
+        if (Input.GetKey("e") && collision.gameObject.layer == 9)
+        {
+            Debug.Log("White");
+            Destroy(this.gameObject);
+        }
+        //Czarny/czerwony
+        if (Input.GetMouseButtonDown(2) && collision.gameObject.layer == 10)
+        {
+            Debug.Log("Black");
+            Destroy(this.gameObject);
+        }
+    }
+}
