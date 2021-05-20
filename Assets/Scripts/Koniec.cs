3472fcd6379388cb04c066f7e3b7d8555f9c88b5
Piotr Strojny <p.strojny@student.po.edu.pl>
2021-05-20 09:52:49 +0200

    MenuGlowne

---
+++ Assets/Scripts/Koniec.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Koniec : MonoBehaviour
+{
+    
+    public void KoniecKlik()
+    {
+        Debug.Log("Wyszedles z gry");
+        Application.Quit();
+    }
+}
