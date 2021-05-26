e621cd0b7f04e2ddfd5a6fe9cae91a295f27faa3
Piotr Strojny <p.strojny@student.po.edu.pl>
2021-05-27 00:33:49 +0200

    Poziom3

---
+++ Assets/Scripts/PilaControler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PilaControler : MonoBehaviour
+{
+    WhiteController white;
+    BlackController black;
+    [SerializeField] float demage = 25f;
+
+    void Awake()
+    {
+        white = FindObjectOfType<WhiteController>();
+        black = FindObjectOfType<BlackController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "White")
+        {
+            var health = white.GetComponent<Health>();
+            health.DealDamage(demage);
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.name == "Black")
+        {
+            var health = black.GetComponent<Health>();
+            health.DealDamage(demage);
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/SpawnePil.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnePil : MonoBehaviour
+{
+    public GameObject pila;
+    [SerializeField] float czasSpawnuWSek = 0f;
+    [SerializeField] float x = 0f;
+    [SerializeField] float y = 0f;
+    float czasa;
+    // Start is called before the first frame update
+    void Start()
+    {
+        czasa = czasSpawnuWSek;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        czasSpawnuWSek -= Time.deltaTime;
+
+        if (czasSpawnuWSek <= 0)
+        {
+            Instantiate(pila, new Vector2(x, y), Quaternion.identity);
+            czasSpawnuWSek = czasa;
+        }
+    }
+
+}
+
