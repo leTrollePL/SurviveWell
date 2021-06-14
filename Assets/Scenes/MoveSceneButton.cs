25b230cf7e74bd9d61ccd8a30892ed2e61bab592
Piotr Strojny <p.strojny@student.po.edu.pl>
2021-06-14 15:50:35 +0200

    PRzenoszeniePoczatek

--- Assets/Scenes/MoveSceneButton.cs
+++ Assets/Scenes/MoveSceneButton.cs
@@ -6,8 +6,36 @@ using UnityEngine.UI;
 
 public class MoveSceneButton : MonoBehaviour
 {
+    private AsyncOperation sceneAsync;
+    public GameObject dane;
+
+    [System.Obsolete]
     public void NewGameTest(string newLevel)
     {
-        SceneManager.LoadScene(newLevel);
+        dane = GameObject.Find("DaneOGrze");
+        AsyncOperation scene = SceneManager.LoadSceneAsync(newLevel, LoadSceneMode.Single);
+        scene.allowSceneActivation = false;
+        sceneAsync = scene;
+
+        while (scene.progress < 0.9f)
+        {
+            Debug.Log("Loading scene " + " [][] Progress: " + scene.progress);
+        }
+        Scene stara = SceneManager.GetActiveScene();
+
+        Debug.Log(stara.name);
+
+        sceneAsync.allowSceneActivation = true;
+        Scene sceneToLoad = SceneManager.GetSceneByName(newLevel);
+        if (sceneToLoad.IsValid())
+        {
+            Debug.Log("Scene is Valid");
+            SceneManager.MoveGameObjectToScene(dane, sceneToLoad);
+            SceneManager.UnloadScene(1);
+            SceneManager.SetActiveScene(sceneToLoad);
+            
+            
+        }
+
     }
 }
---
+++ Assets/Scripts/DaneOGrze.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaneOGrze : MonoBehaviour
+{
+
+    public double hpBialy = 100;
+    public double hpCzasrny = 100;
+
+    public double hajsBialy = 0;
+    public double hajsCzasrny = 0;
+
+    public bool buffBialy = false;
+    public bool buffCzarny = false;
+
+    public double poziomGlosnosci;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
